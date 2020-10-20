5702d12ad09e63d447c6ec4008c3dc50e1f70387
Alba Domi <adomi@km3net.de>
2020-10-20 13:59:31 +0200

    remove file

--- test_function.py
+++
@@ -1,9 +0,0 @@
-def get_selected_items(point, item, d_min, d_max):
-
-        item_pos = np.array([item.pos_x, item.pos_y, item.pos_z]).T
-        distances = kp.math.dist(point, item_pos, axis=1)
-        mask = (distances >= d_min) & (distances <= d_max)
-
-        selected_items = item[mask]
-  
-        return selected_items
